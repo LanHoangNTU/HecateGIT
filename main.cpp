91c5e2753fee18f461e14ae456851519a33889da
NguyenDinhHoangLan <63280056+LanHoangNTU@users.noreply.github.com>
2022-02-27 15:52:45 +0700

    Update main.cpp
    - Use window class to create SDL window

--- main.cpp
+++ main.cpp
@@ -1,46 +1,19 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
+#include <iostream>
+#include <memory>
+#include <hecate/window.h>
 
 int main(int argc, char** argv) {
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
-        printf("Error: SDL failed to initialize\n Error: '%s'\n", SDL_GetError());
-        return 1;
+
+    try {
+        std::unique_ptr<Window> win = std::make_unique<Window>(Window());
+        win->Size(1366, 786);
+        win->CreateWindow();
+        win->Start();
+        win->Quit();
+    } catch (char const* error) {
+        std::cout << error;
     }
-
-    SDL_Window *window = SDL_CreateWindow("HecateGIT", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, 0);
-    if(!window){
-        printf("Error: Failed to open window\n Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        printf("Error: Failed to create renderer\n Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
-
-    bool running = true;
-    while(running){
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            switch(event.type){
-                case SDL_QUIT:
-                    running = false;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 
     return 0;
 }
